3a9909dd06f80cc6a09d4953a409221142116e79
sunwxg <sun.wxg@gmail.com>
2015-11-17 15:57:06 +0800

    add TestMain func

--- golibwireshark_test.go
+++ golibwireshark_test.go
@@ -3,55 +3,67 @@ package golibwireshark_test
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/sunwxg/golibwireshark"
 )
 
-func TestIsKey(t *testing.T) {
+var p golibwireshark.Packet
+
+func TestMain(m *testing.M) {
 	file := "1.pcap"
 	err := golibwireshark.Init(file, "o.pcap")
 	if err != nil {
-		t.Fatalf("%s", err)
+		fmt.Println("open file error")
+		os.Exit(1)
 	}
-
-	var p golibwireshark.Packet
 
 	p.GetPacket()
 	if p.Edt == nil {
-		t.Fatalf("don't find packet\n")
+		fmt.Println("can't find packet")
+		os.Exit(1)
 	}
 
-	key := "ip.addr"
-	get, _ := p.Iskey(key)
-
-	//fmt.Println(p)
-	buf := make([]byte, 100, 200)
-	w := bytes.NewBuffer(buf)
-
-	if p.GetField("ip") {
-		//fmt.Println(p)
-		fmt.Fprintln(w, p)
-	}
-
-	err = p.WriteToFile()
-	if err != nil {
-		t.Errorf("%s", err)
-	}
+	m.Run()
 
 	p.FreePacket()
 
 	golibwireshark.Clean()
+
+}
+
+func TestIsKey(t *testing.T) {
+	key := "ip.addr"
+	get, _ := p.Iskey(key)
 
 	wanted := "10.128.229.6"
 	if get != wanted {
 		t.Fatalf("wanted: (%s), get: (%s)", wanted, get)
 	}
 
-	//fmt.Println(w)
+}
+
+func TestGetField(t *testing.T) {
+	buf := make([]byte, 100, 200)
+	w := bytes.NewBuffer(buf)
+
+	if p.GetField("ip") {
+		fmt.Fprintln(w, p)
+	}
+
 	len := w.Len()
 	wanted_len := 662
 	if len != wanted_len {
 		t.Fatalf("wanted: (%d), get: (%d)", wanted_len, len)
 	}
+}
+
+func TestWriteToFile(t *testing.T) {
+
+	err := p.WriteToFile()
+	if err != nil {
+		t.Errorf("%s", err)
+	}
+
 }
